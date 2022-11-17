4d5749c5ffe94abbb225c7a15421f821e9527f44
HatmanW <118311932+HatmanW@users.noreply.github.com>
2022-11-17 15:38:58 -0600

    added type hinting, and doc strings upoading test_account.py to test branch

--- account.py
+++ account.py
@@ -2,32 +2,64 @@
 
 class Account:
 
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
+        '''
+        This function is supposed to initialize the
+        variables for account name (acct_name),
+        and account balance (acct_Bal).
+        :param name: name of account holder.
+        '''
+        self.__acct_name = name
+        self.__acct_bal = 0
 
-        self.acct_name = name
-        self.acct_bal = 0
 
+    def deposit(self, amount: float) -> bool:
+        '''
+        The point of this function is to deposit.
+        If the amount entered is less than or equal to 0,
+        nothing will happen.
+        If the amount entered is a positive whole number,
+        it will add the amount to the account balance.
 
-    def deposit(self, amount):
-
+        :param amount: This is the amount entered.
+        :return: Result of calculation.
+        '''
         if amount <=0:
             return False
-        self.acct_bal += amount
-        return True
+        elif amount > 0:
+            self.__acct_bal += amount
+            return True
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float) -> bool:
+        '''
+        This Function is to withdraw.
+        If the amount entered is less than or equal to 0,
+        OR the amount is greater than the current account
+        balance, nothing happens.
+        If the amount is within our account balance range,
+        it will subtract the ammount from our account balance
 
-        if amount <= 0 or amount > self.acct_bal:
+        :param amount: Amount to subtract from the account.
+        :return: Result of calculation.
+        '''
+        if amount <= 0 or amount > self.__acct_bal:
             return False
-        self.acct_bal -= amount
-        return True
+        elif amount > 0:
+            self.__acct_bal -= amount
+            return True
 
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+        '''
+        To get the current account balance.
+        :return: Current account balance.
+        '''
+        return self.__acct_bal
 
-        return self.acct_bal
 
-
-    def get_name(self):
-
-        return self.acct_name
+    def get_name(self) -> str:
+        '''
+        To get the account name.
+        :return: Current account name.
+        '''
+        return self.__acct_name
---
+++ test_account.py
@@ -0,0 +1,52 @@
+import pytest
+from account import *
+
+class Test:
+    def setup_method(self):
+        self.a1 = Account('John')
+        self.a2 = Account('Jane')
+
+    def tearDown(self):
+        del self.a1
+        del self.a2
+
+
+    def test_init_(self):
+        assert self.a1.get_name() =='John'
+        assert self.a1.get_balance() == 0
+        assert self.a2.get_name() == 'Jane'
+        assert self.a2.get_balance() == 0
+
+
+    def test_deposit(self):
+        assert self.a1.deposit(-2) == False
+        assert self.a1.get_balance() == 0
+        assert self.a1.deposit(2) == True
+        assert self.a1.get_balance() == 2
+        assert self.a1.deposit(0) == False
+        assert self.a1.get_balance() == 2
+
+        assert self.a2.deposit(-2) == False
+        assert self.a2.get_balance() == 0
+        assert self.a2.deposit(5) == True
+        assert self.a2.get_balance() == 5
+        assert self.a2.deposit(0) == False
+        assert self.a2.get_balance() == 5
+
+
+    def test_withdraw(self):
+        assert self.a1.withdraw(-2) == False
+        assert self.a1.get_balance() == 0
+        self.a1.deposit(5)
+        assert self.a1.withdraw(2) == True
+        assert self.a1.get_balance() == 3
+        assert self.a1.withdraw(0) == False
+        assert self.a1.get_balance() == 3
+
+        assert self.a2.withdraw(-2) == False
+        assert self.a2.get_balance() == 0
+        self.a2.deposit(6)
+        assert self.a2.withdraw(3) == True
+        assert self.a2.get_balance() == 3
+        assert self.a2.withdraw(0) == False
+        assert self.a2.get_balance() == 3
